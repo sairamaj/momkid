1730a2248a2332db7ddfd5f538afd89dd1a87eb6
sairama <sairamaj@hotmail.com>
2016-12-28 10:47:56 -0800

    fixing the orders function

--- getOrders.js
+++ getOrders.js
@@ -1,24 +1,26 @@
 exports.handler = function (event, context, callback) {
 
     var momRestaruant = require("./momrestaruant")
+    var util = require("./util")
 
     momRestaruant.getOrders( function(err,orders){
         if( err){
             console.log(err)
+            response = util.createResponse(500, err);
+            context.fail(response);
         }else{
-            callback(null,orders)
+            response = util.createResponse(200, orders);
+            context.succeed(response);
         }
     })
 }
 
 function sampleTest(){
-    exports.handler(null, null,function(err,response){
-        if(err){
-            console.log(err)
-        }else{
-            console.log(response)
-        }
-    })
+    var context = {}
+    context.succeed = function(resp){
+        console.log(resp)
+    }
+    exports.handler(null, context,null)
 }
 
 //sampleTest()
