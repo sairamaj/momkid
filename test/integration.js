e6bb25d63ce6189903d216fd2f1e08428957355d
sairama <sairamaj@hotmail.com>
2016-12-28 16:03:11 -0800

    introducing testing

---
+++ test/integration.js
@@ -0,0 +1,21 @@
+var should = require('should');
+
+describe('getMenuItems', function () {
+
+    before(function (done) {
+        console.log("in before")
+        done()
+    });
+
+    after(function (done) {
+        console.log("in after")
+        done()
+    });
+
+    it('should get more than one menu item.', function (done) {
+        console.log("in test");
+        var items = ["item1","item2"]
+        items.length.should.eql(3)
+        done();
+    });
+});
